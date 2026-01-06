bd41e86c4ea4f313655dc745ad00a5548081cb4c
Aditya Kumar Barnwal <adityakumarbarnwal2005@gmail.com>
2026-01-06 18:00:18 +0530

    Use environment variable for MongoDB URI
    for backend deployment

--- 03_module_a/A8/config/db.js
+++ 03_module_a/A8/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectDB=async ()=>{
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("mongodb is connected");
 }
 
