e56c39298fb5bcae44e6b423726f862b79440924
Aditya Kumar Barnwal <adityakumarbarnwal2005@gmail.com>
2026-01-06 17:57:00 +0530

    Update MongoDB connection string to use MONGODB_URI
    for backend connection approved by Amit sir

--- 03_module_a/A8/config/db.js
+++ 03_module_a/A8/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectDB=async ()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/users');
+    await mongoose.connect(MONGODB_URI);
     console.log("mongodb is connected");
 }
 
